2421e8384c635143dbe9c03595ecd7f1756174bb
geshrifter <91565763+geshrifter@users.noreply.github.com>
2021-10-04 15:02:42 -0500

    Add files via upload

---
+++ circle.py
@@ -0,0 +1,18 @@
+#Initialization
+import turtle
+import random
+grace = turtle.Turtle()
+
+#Functions
+def randomDot() :
+    grace.color("blue")
+    grace.begin_fill()
+    grace.circle(100)
+    grace.end_fill()
+
+#Main
+randomDot()
+
+
+
+    
